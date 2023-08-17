19110dfa79c382b5465d6dfc97bd715d75e0e847
Gilnario-Saraiva <gilnario@yahoo.com.br>
2023-08-16 22:14:50 -0300

    criando requisições

--- agenda/src/main/java/model/DAO.java
+++ agenda/src/main/java/model/DAO.java
@@ -1,5 +1,27 @@
 package model;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DAO {
+	/**Módulo de conexão**/
+	//Parametro de conexão
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://localhost:3306/dbagenda?serverTimezone=America/Sao_Paulo";
+	private String user = "root";
+	private String password = "banco1.0";
+		
+	//Método de conexão
+	private Connection conectar() {
+		Connection con = null;
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password);
+			return con;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return null;
+		}
+	}
+	
 }
