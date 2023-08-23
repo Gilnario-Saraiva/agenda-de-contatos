ff04e3d7b54a34f562f4bc9cdb19f4988eb6448c
Gilnario-Saraiva <gilnario@yahoo.com.br>
2023-08-22 21:37:53 -0300

    enviando a requisição de novo contato para a classe controller

--- agenda/src/main/java/controller/Controller.java
+++ agenda/src/main/java/controller/Controller.java
@@ -7,11 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.DAO;
+import model.JavaBeans;
 
-@WebServlet(urlPatterns = { "/Controller", "/main" })
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
+	JavaBeans contato = new JavaBeans();
 
 	public Controller() {
 		super();
@@ -20,18 +22,34 @@ public class Controller extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// response.getWriter().append("Served at: ").append(request.getContextPath());
 		String action = request.getServletPath();
 		System.out.println(action);
+
 		if (action.equals("/main")) {
 			contatos(request, response);
+		} else if (action.equals("/insert")) {
+			novoContato(request, response);
+		} else {
+			response.sendRedirect("index.html");
 		}
 	}
-
+	
 	// Listar Contatos
 	protected void contatos(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.sendRedirect("agenda.jsp");
 	}
+	
+	// Novo contato
+		protected void novoContato(HttpServletRequest request, HttpServletResponse response)
+				throws ServletException, IOException {
+			System.out.println(request.getParameter("nome"));
+			System.out.println(request.getParameter("fone"));
+			System.out.println(request.getParameter("email"));
+			//setar variaveis javabeans
+			contato.setNome(request.getParameter("nome"));
+			contato.setFone(request.getParameter("fone"));
+			contato.setEmail(request.getParameter("email"));
+		}
 
 }
